e0d6c5cbc262647ae72c3d8313489c8c74522084
deluardon <160032360+deluardon@users.noreply.github.com>
2024-08-30 01:54:32 +0600

    condition if else

--- if-else.js
+++ if-else.js
@@ -1,11 +1,20 @@
-var price = 100;
+// var price = 100;
 
-if (price >100){
-    console.log("amar kache 100 taka che ");
+// if (price >100){
+//     console.log("amar kache 100 taka che ");
+// }
+// else if (price >= 100){
+//     console.log("amar kache 100 aka nai and er besio nai ");
+// }
+//  else{
+//     console.log("amar kache 100 taka nai ba er kom o nai ");
+// }
+
+
+var data=20;
+if(data<=20){
+    console.log("amar data 10 er beshi");
 }
-else if (price >= 100){
-    console.log("amar kache 100 aka nai and er besio nai ");
-}
- else{
-    console.log("amar kache 100 taka nai ba er kom o nai ");
+else{
+    console.log("amar data 10 er kom");
 }
---
+++ mittple.js
@@ -0,0 +1,23 @@
+const slary=35000;
+const isBcs=true;
+// if(slary>30000 && isBcs > true){
+// console.log("tumi pas kore gacho");
+// }
+// else{
+//     console.log("tumi pas kore gacho na");
+// }
+
+// if(slary>30000 || isBcs > true){
+//     console.log("tumi pas kore gacho");
+// }
+// else{
+//     console.log("tumi pas kore gacho na");
+// }
+
+const Hight=60;
+if((slary>30000 && isBcs > true) || Hight>55){
+    console.log("tumi pas kore gacho");
+}
+else{
+    console.log("tumi pas kore gacho na");
+}
