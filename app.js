3a398352ec3f5e98e2ee034d20b12fc6363b9264
deluardon <160032360+deluardon@users.noreply.github.com>
2024-08-30 00:51:42 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// let x=10;
+// let y=11;
+// let z= x>y;
+// console.log(z);
+
+// console.log(10 < 12);
+// console.log(10 > 12);
+// console.log(10 == 10);
+
+
+var price=500;
+var apple=100;
+var x=price>apple;
+console.log(x);
