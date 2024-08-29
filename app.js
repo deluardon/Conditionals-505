790c81f6d077de11730e742c73853e0e755e1803
deluardon <160032360+deluardon@users.noreply.github.com>
2024-08-30 01:11:23 +0600

    js condition

--- app.js
+++ app.js
@@ -8,7 +8,14 @@
 // console.log(10 == 10);
 
 
-var price=500;
-var apple=100;
-var x=price>apple;
-console.log(x);
+var price = 500;
+var apple = 100;
+var x = price > apple;
+// console.log(x);
+
+// console.log(10 == 10);
+// console.log(10 == "10");
+// console.log(10 === 10);
+// console.log(10 === "10");
+// console.log(10 != 10);
+console.log(10 !== '10');
---
+++ if-else.js
@@ -0,0 +1,11 @@
+var price = 100;
+
+if (price >100){
+    console.log("amar kache 100 taka che ");
+}
+else if (price >= 100){
+    console.log("amar kache 100 aka nai and er besio nai ");
+}
+ else{
+    console.log("amar kache 100 taka nai ba er kom o nai ");
+}
